6e6a61f17d201de863ccbe47424cdb7c904179df
ytnobody <ytnobody@gmail.com>
2015-03-20 12:53:58 +0900

    add docs

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # NAME
 
-Qlover - It's new $module
+Qlover - Analyse with Google BigQuery. Then, Store these into Elasticsearch.
 
 # SYNOPSIS
 
-    use Qlover;
+Bootup the worker first.
+
+    $ ./bin/qlover_worker.pl
+
+Then, plackup the API server.
+
+    $ plackup ./api.psgi
+
+You can post a request with query for BigQuery. If you do so, qlover\_worker.pl create docs into Elasticsearch automatically.
+
+    $ curl -XPOST http://localhost:5000/yourindex/yourtype -F 'query=SELECT * FROM yourdataset.yourtable LIMIT 10;'
 
 # DESCRIPTION
 
-Qlover is ...
+Qlover is a transferring middleware BigQuery and Easticsearch.
 
 # LICENSE
 
--- lib/Qlover.pm
+++ lib/Qlover.pm
@@ -5,8 +5,6 @@ use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
 __END__
 
@@ -14,15 +12,25 @@ __END__
 
 =head1 NAME
 
-Qlover - It's new $module
+Qlover - Analyse with Google BigQuery. Then, Store these into Elasticsearch.
 
 =head1 SYNOPSIS
 
-    use Qlover;
+Bootup the worker first.
+
+    $ ./bin/qlover_worker.pl
+
+Then, plackup the API server.
+
+    $ plackup ./api.psgi
+
+You can post a request with query for BigQuery. If you do so, qlover_worker.pl create docs into Elasticsearch automatically.
+
+    $ curl -XPOST http://localhost:5000/yourindex/yourtype -F 'query=SELECT * FROM yourdataset.yourtable LIMIT 10;'
 
 =head1 DESCRIPTION
 
-Qlover is ...
+Qlover is a transferring middleware BigQuery and Easticsearch.
 
 =head1 LICENSE
 
